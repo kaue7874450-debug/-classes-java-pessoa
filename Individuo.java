b484c967b871bcb043c3cdc1e50eb58fb702be4c
kaue7874450-debug <kaue.7874450@aluno.mg.gov.br>
2026-04-10 10:27:46 -0300

    Create  Individuo.java

---
+++ Individuo.java
@@ -0,0 +1,12 @@
+package [seunome];
+
+public class Pessoa {
+String kaue;
+int 16;
+        public void caminhar() {
+        System.out.println("caminhando...");
+        }
+        public void dormir() {
+        System.out.println("dormir...");
+        }
+}
